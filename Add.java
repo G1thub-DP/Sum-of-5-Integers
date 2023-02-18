058bbeeadcb4d10cc027b3c740d5d71130e01d08
D. Devi Prasad <118455529+G1thub-DP@users.noreply.github.com>
2023-02-18 20:47:57 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,18 @@
+package integersSum;
+
+public class Add {
+
+	public static void main(String[] args) {
+		int n1=10;
+		int n2=20;
+		int n3=30;
+		int n4=40;
+		int n5=50;
+		
+		int sum = n1+n2+n3+n4+n5;
+		System.out.println("The sum of 5 numbers is: "+sum);
+		
+
+	}
+
+}
